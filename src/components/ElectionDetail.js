35b5c2e9db8fc49103fc2ee8f842fb66cc7116b4
virmon <virmontumulak@gmail.com>
2020-03-30 16:55:56 -0500

    setup routes

---
+++ src/components/ElectionDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ElectionDetail extends Component {
+    render() {
+        return (
+            <div>
+                Election Detail Page
+            </div>
+        );
+    }
+}
+
+export default ElectionDetail;
---
+++ src/components/Results.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+    render() {
+        return (
+            <div>
+                Results Page
+            </div>
+        );
+    }
+}
+
+export default Results;
