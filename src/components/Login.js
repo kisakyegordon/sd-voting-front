1a39db128dd62866013220730c49b8df038b9848
virmon <virmontumulak@gmail.com>
2020-03-30 22:20:12 -0500

    add login and register UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import logo from '../logo.svg';
 
 class Login extends Component {
+    state = {
+        toHome: false
+    }
+
+    handleSignIn (e) {
+        e.preventDefault();
+
+        // validate user
+
+        // set authed user
+
+        this.setState({
+            toHome: true
+        })
+    }
+
     render() {
+        if (this.state.toHome === true) {
+            let redirectUrl = '/elections';
+            return <Redirect to={redirectUrl} />
+        }
         return (
-            <div>
-                Login Page
+            <div className='login'>
+                <img src={logo} alt='logo' />
+                <input className='form-input' type='text' placeholder='email'/>
+                <input className='form-input' type='password' placeholder='password'/>
+                <input className='form-btn' onClick={(e) => this.handleSignIn(e)} type='submit' value='Sign in' />
             </div>
         );
     }
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,13 @@ class Register extends Component {
     render() {
         return (
             <div>
-                Register Page
+                <div className='register'>
+                    <h2>Register</h2>
+                    <input className='form-input' type='text' placeholder='name'/>
+                    <input className='form-input' type='email' placeholder='email'/>
+                    <input className='form-input' type='password' placeholder='password'/>
+                    <input className='form-btn' type='submit' value='Sign up' />
+                </div>
             </div>
         );
     }
