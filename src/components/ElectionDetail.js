48e6219c800bcd8bbca09914645e6eae5e8898f9
elgordo <elgordo@Elgordos-MacBook-Pro.local>
2020-03-31 22:55:05 -0500

    add dummy data

--- src/components/ElectionDetail.js
+++ src/components/ElectionDetail.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import NavBar from './common/NavBar';
 import LogoPane from './common/LogoPane';
+import Card from './common/Card';
+import { elections } from './sampleData';
 
 class ElectionDetail extends Component {
+
+    state = {
+        checked: null
+    }
+
+    handleSelect = id => {
+        this.setState({ checked: id})
+    }
+
+    handleVote = () => {
+        console.log("Voted: ", this.state.checked);
+        this.props.history.push('/results');
+    } 
+
     render() {
+        const electionDetail = elections.filter(item => item.id == this.props.match.params.id);
         return (
             <div>
                 <LogoPane />
                 <NavBar title="Available Elections"/>
                 <div className='main-content'>
-                    Election Detail Page
+                    <div>
+                    <ul>
+                        {
+                            electionDetail[0].vote_options.map( 
+                                (item, index) => 
+                                <li key={index}>
+                                    <Card candidate={item} />
+                                    <input style={{width: "40px"}} checked={this.state.checked === item.id} type='checkbox' onChange={() => this.handleSelect(item.id)} />
+                                </li> )
+                        }
+                    </ul>
+                    <div className='vote-button'> 
+                        <button onClick={this.handleVote} className='log-out-button'>Vote</button>
+                     </div>
+                     </div>
                 </div>
             </div>
         );
     }
 }
 
-export default ElectionDetail;
+export default withRouter(ElectionDetail);
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import NavBar from './common/NavBar';
 import LogoPane from './common/LogoPane';
+import Card from './common/Card';
+import {elections} from "./sampleData";
+
 
 class Results extends Component {
     render() {
@@ -9,7 +12,21 @@ class Results extends Component {
                 <LogoPane />
                 <NavBar title="Available Elections"/>
                 <div className='main-content'>
-                    Results Page
+                    <div className='main-content'>
+                    <div>
+                    <ul>
+                        {
+                            elections[0].vote_options.map( 
+                                (item, index) => 
+                                <li key={index}>
+                                    <h4>{index + 1}</h4>
+                                    <Card candidate={item} />
+                                    <h4>30 Votes</h4>
+                                </li> )
+                        }
+                    </ul>
+                     </div>
+                </div>
                 </div>
             </div>
         );
